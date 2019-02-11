1e3933a3467297752375023becf4baf4e65cb2c0
rahulanddipika <47480826+rahulanddipika@users.noreply.github.com>
2019-02-11 14:51:33 +0530

    countdown

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2019,
+                month: 4,
+                day: 12,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
